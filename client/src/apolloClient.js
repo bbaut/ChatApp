83588d27fc991eb8726df36cbfac3b164b3293fd
BranBautista <bbrandon.abo@gmail.com>
2023-05-03 17:39:08 -0600

    set up proxy

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -3,13 +3,6 @@ import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
     uri: "http://localhost:4000/graphql",
-    headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'POST, PATCH, OPTIONS, GET, REMOVE',
-        'Access-Control-Allow-Credentials': true,
-    },
-    fetch
 })
 
 const client = new ApolloClient ({
