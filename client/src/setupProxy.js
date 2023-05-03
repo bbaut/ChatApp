c987af85bfa9ee58989d73be96ad76303726d3b6
BranBautista <bbrandon.abo@gmail.com>
2023-05-03 17:41:50 -0600

    proxy

---
+++ client/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+    app.use(
+        createProxyMiddleware("/graphql", {
+            target: "http://localhost:4000/graphql",
+            changeOrigin: true,
+            pathRewrite: (path) => path.replace(/^\/graphql/, ""),
+        })
+    );
+};
