9f44465d3082e188f8ba6e912158cf929d3122fe
BranBautista <bbrandon.abo@gmail.com>
2023-05-09 18:22:18 -0600

    chat service

---
+++ chat_service/db/config.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const dbConnection = async () => {
+    const MONGO_HOST = "localhost"
+    const MONGO_DB = "db_chat"
+
+    const URI = `mongodb://${MONGO_HOST}/${MONGO_DB}`;
+    
+    try {
+        await mongoose.connect(URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        console.log('🌿 Data base successfully connected');
+    }
+    catch (error) {
+        console.log(error);
+        throw new Error('Error with the db')
+    }
+}
+
+export default dbConnection;
---
+++ chat_service/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from 'dotenv';
+import dbConnection from "./db/config.js";
+import chatRoutes from "./routes/chatRoutes.js";
+
+//configure the use of environment variables
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT;
+const chatRoutePath = '/api/chat';
+
+dbConnection();
+
+app.use(chatRoutePath, chatRoutes);
+
+app.listen(port, () => {
+    console.log(`🚀 Rest server listening on ${port}`)
+})
