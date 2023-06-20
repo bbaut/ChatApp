7c8525279724ee90f8030334df933e285b2b1c65
BranBautista <bbrandon.abo@gmail.com>
2023-06-20 14:31:20 -0600

    k8s

--- client/src/apolloClient.js
+++ client/src/apolloClient.js
@@ -3,14 +3,28 @@ import { getMainDefinition } from '@apollo/client/utilities';
 import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
 import { createClient } from 'graphql-ws';
 
+
+const SERVER_URI = "/graphql";
+const WS_URL =
+   window.location.protocol === "https:"
+    ? `wss://${window.location.host}/graphql`
+    : `ws://${window.location.host}/graphql`;
+
+
 const wsLink = new GraphQLWsLink(createClient({
-    url: 'ws://localhost/subscription',
+    // url: 'ws://localhost/subscription',
     // url: 'ws://localhost:4000/graphql',
+    url: "ws://chat-app.brandon/graphql",
+    connectionParams: {
+      credentials: "same-origin",
+    },
   }));
 
 const httpLink = new HttpLink({
-    uri: "http://localhost/graphql",
+    // uri: "http://localhost/graphql",
     // uri: "http://localhost:4000/graphql",
+      uri: "http://chat-app.brandon/graphql",
+      credentials: "same-origin",
 })
 
 const splitLink = split(
